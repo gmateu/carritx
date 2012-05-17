ab72f55d5c127d512fbd424f800a38595fb1cb37
guillem <guillem@direccio.(none)>
2012-05-17 12:30:29 +0200

    afegida templates pròpies de polls

--- carritx/settings.py
+++ carritx/settings.py
@@ -106,7 +106,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/guillem/django_templates"
+    "/home/guillem/django_templates",
+    "/home/guillem/django_projects/carritx/templates",
 )
 
 INSTALLED_APPS = (
