f41ec838165a0e76f6fc3845ebee1879f4a5a5cd
guillem mateu <gmateu@esliceu.com>
2012-05-17 12:58:13 +0200

    tutorial 3 acabat

--- carritx/urls.py
+++ carritx/urls.py
@@ -1,24 +1,9 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'carritx.views.home', name='home'),
-    # url(r'^carritx/', include('carritx.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    
-    #polls
-    url(r'^polls/$', 'polls.views.index'),
-    url(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
-    url(r'^polls/(?P<poll_id>\d+)/(?P<choice_id>\d+)$', 'polls.views.prova'),
-    url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
-    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
-
-    # Uncomment the next line to enable the admin:
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
